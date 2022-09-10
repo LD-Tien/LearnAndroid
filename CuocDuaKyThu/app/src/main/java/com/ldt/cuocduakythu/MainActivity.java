51722cb2d60b4ed2d59ae2761284bcbe7100a9f2
LD-Tien <leductien00@gmail.com>
2022-09-10 18:34:39 +0700

    Update phần về đích

--- CuocDuaKyThu/app/src/main/java/com/ldt/cuocduakythu/MainActivity.java
+++ CuocDuaKyThu/app/src/main/java/com/ldt/cuocduakythu/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -30,11 +31,30 @@ public class MainActivity extends AppCompatActivity {
         CountDownTimer countDownTimer = new CountDownTimer(60000, 1) {
             @Override
             public void onTick(long l) {
-                int number = 2;
+                int number = 3;
                 Random random = new Random();
                 int one = random.nextInt(number);
                 int two = random.nextInt(number);
                 int three = random.nextInt(number);
+
+                // Kiểm tra ai về trước
+                if(sbOne.getProgress() >= sbOne.getMax()) {
+                    this.cancel();
+                    ibtnPlay.setVisibility(View.VISIBLE);
+                    Toast.makeText(MainActivity.this, "ONE WIN", Toast.LENGTH_SHORT).show();
+                }
+
+                if(sbTwo.getProgress() >= sbTwo.getMax()) {
+                    this.cancel();
+                    ibtnPlay.setVisibility(View.VISIBLE);
+                    Toast.makeText(MainActivity.this, "TWO WIN", Toast.LENGTH_SHORT).show();
+                }
+
+                if(sbThree.getProgress() >= sbThree.getMax()) {
+                    this.cancel();
+                    ibtnPlay.setVisibility(View.VISIBLE);
+                    Toast.makeText(MainActivity.this, "THREE WIN", Toast.LENGTH_SHORT).show();
+                }
                 sbOne.setProgress(sbOne.getProgress() + one);
                 sbTwo.setProgress(sbTwo.getProgress() + two);
                 sbThree.setProgress(sbThree.getProgress() + three);
@@ -50,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ibtnPlay.setVisibility(View.INVISIBLE);
+                sbOne.setProgress(0);
+                sbTwo.setProgress(0);
+                sbThree.setProgress(0);
                 countDownTimer.start();
             }
         });
