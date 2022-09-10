a09a9dcd825097f09b56dcc5850f8cb57d3efcf4
LD-Tien <leductien00@gmail.com>
2022-09-10 19:06:51 +0700

    Update đặt cược và tính điểm

--- CuocDuaKyThu/app/src/main/java/com/ldt/cuocduakythu/MainActivity.java
+++ CuocDuaKyThu/app/src/main/java/com/ldt/cuocduakythu/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
     TextView txtDiem;
     ImageButton ibtnPlay;
     RadioButton cbOne, cbTwo, cbThree;
+    RadioGroup radioGroup;
     SeekBar sbOne, sbTwo, sbThree;
+    int soDiem = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +32,63 @@ public class MainActivity extends AppCompatActivity {
 
         anhXa();
 
+        txtDiem.setText(soDiem + "");
+
         CountDownTimer countDownTimer = new CountDownTimer(60000, 1) {
             @Override
             public void onTick(long l) {
-                int number = 3;
+                int number = 2;
                 Random random = new Random();
                 int one = random.nextInt(number);
                 int two = random.nextInt(number);
                 int three = random.nextInt(number);
+
+
 
                 // Kiểm tra ai về trước
                 if(sbOne.getProgress() >= sbOne.getMax()) {
                     this.cancel();
                     ibtnPlay.setVisibility(View.VISIBLE);
                     Toast.makeText(MainActivity.this, "ONE WIN", Toast.LENGTH_SHORT).show();
+                    // Kiểm tra đặt cược
+                    if(cbOne.isChecked()) {
+                        soDiem += 10;
+                        Toast.makeText(MainActivity.this, "Bạn đoán chính xác", Toast.LENGTH_SHORT).show();
+                    } else {
+                        soDiem -= 5;
+                        Toast.makeText(MainActivity.this, "Bạn đoán sai rồi", Toast.LENGTH_SHORT).show();
+                    }
+                    txtDiem.setText(soDiem + "");
                 }
 
                 if(sbTwo.getProgress() >= sbTwo.getMax()) {
                     this.cancel();
                     ibtnPlay.setVisibility(View.VISIBLE);
                     Toast.makeText(MainActivity.this, "TWO WIN", Toast.LENGTH_SHORT).show();
+                    // Kiểm tra đặt cược
+                    if(cbTwo.isChecked()) {
+                        soDiem += 10;
+                        Toast.makeText(MainActivity.this, "Bạn đoán chính xác", Toast.LENGTH_SHORT).show();
+                    } else {
+                        soDiem -= 5;
+                        Toast.makeText(MainActivity.this, "Bạn đoán sai rồi", Toast.LENGTH_SHORT).show();
+                    }
+                    txtDiem.setText(soDiem + "");
                 }
 
                 if(sbThree.getProgress() >= sbThree.getMax()) {
                     this.cancel();
                     ibtnPlay.setVisibility(View.VISIBLE);
                     Toast.makeText(MainActivity.this, "THREE WIN", Toast.LENGTH_SHORT).show();
+                    // Kiểm tra đặt cược
+                    if(cbThree.isChecked()) {
+                        soDiem += 10;
+                        Toast.makeText(MainActivity.this, "Bạn đoán chính xác", Toast.LENGTH_SHORT).show();
+                    } else {
+                        soDiem -= 5;
+                        Toast.makeText(MainActivity.this, "Bạn đoán  sai rồi", Toast.LENGTH_SHORT).show();
+                    }
+                    txtDiem.setText(soDiem + "");
                 }
                 sbOne.setProgress(sbOne.getProgress() + one);
                 sbTwo.setProgress(sbTwo.getProgress() + two);
@@ -69,11 +104,15 @@ public class MainActivity extends AppCompatActivity {
         ibtnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ibtnPlay.setVisibility(View.INVISIBLE);
-                sbOne.setProgress(0);
-                sbTwo.setProgress(0);
-                sbThree.setProgress(0);
-                countDownTimer.start();
+                if(cbOne.isChecked() || cbTwo.isChecked() || cbThree.isChecked()) {
+                    ibtnPlay.setVisibility(View.INVISIBLE);
+                    sbOne.setProgress(0);
+                    sbTwo.setProgress(0);
+                    sbThree.setProgress(0);
+                    countDownTimer.start();
+                } else {
+                    Toast.makeText(MainActivity.this, "Vui lòng đặt cược trước khi chơi", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -81,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
     private void anhXa() {
         txtDiem     = (TextView) findViewById(R.id.textviewDiem);
         ibtnPlay    = (ImageButton) findViewById(R.id.imagePlay);
+        radioGroup  = (RadioGroup) findViewById(R.id.radioGroup);
         cbOne       = (RadioButton) findViewById(R.id.checkBoxCharacter1);
         cbTwo       = (RadioButton) findViewById(R.id.checkBoxCharacter2);
         cbThree     = (RadioButton) findViewById(R.id.checkBoxCharacter3);
